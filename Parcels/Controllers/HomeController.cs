e8a8769e860cd252820ccd1050d898c209b0fa25
Dana <dcluukko@gmail.com>
2020-03-10 17:37:18 -0700

    working through error messages

--- Parcels/Controllers/HomeController.cs
+++ Parcels/Controllers/HomeController.cs
@@ -1,16 +1,34 @@
-using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 using Parcels.Models;
 
 namespace Parcels.Controllers
 {
-  public class HomeController : Controller
+  public class ParcelController : Controller
   {
-
     [HttpGet("/")]
-    public ActionResult Index()
+    public ActionResult Home() { return View(); }
+
+    [HttpGet("AddParcelForm")]
+    public ActionResult AddParcelForm() { return View(); }
+
+    [HttpPost("/DisplayParcel")]
+    public ActionResult AddParcelForm(int length, int width, int weight, int height)
     {
-      return View();
+      Parcel newParcel = new Parcel(length, width, weight, height);
+      int volume = newParcel.GetVolume();
+      double cost = newParcel.GetCost();
+      return RedirectToAction("DisplayParcel");
+    }
+    [HttpGet("/DisplayParcel")]
+    public ActionResult DisplayParcel()
+    {
+      List<Parcel> newParcelList = Parcel.ShowParcel();
+      return View(newParcelList);
     }
   }
 }
